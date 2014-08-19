b25733bc0c6bb7d6f43e3f11563aecc127be8126
Amaresh Pattanaik <apattanaik@paypal.com>
2014-08-19 14:32:42 -0500

    do a google search and take screenshot.

--- src/test/java/net/qatrend/test/GoogleTest.java
+++ src/test/java/net/qatrend/test/GoogleTest.java
@@ -1,14 +1,17 @@
 package net.qatrend.test;
 
+import java.io.File;
 import java.net.URL;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class GoogleTest {
@@ -41,8 +44,18 @@ public class GoogleTest {
 			driver.get("http://google.com");
 			WebElement element = driver.findElement(By.ByCssSelector
 					.cssSelector("input[name='q']"));
-			Thread.sleep(10000);
-			System.out.println("style: " + element.getAttribute("style"));
+			Thread.sleep(3000);
+			//check that my name appears on the first page when I search for visitamaresh
+			element.sendKeys("visitamaresh.com");
+			element.submit();
+			Assert.assertTrue(driver.getPageSource().contains("Amaresh Pattanaik"));
+			File tempScreenshot = driver.getScreenshotAs(OutputType.FILE);
+			System.out.println("location of screenshot file: " + tempScreenshot.getPath());
+			
+			File screenshot = new File("test-output/amaresh.png");
+			FileUtils.deleteQuietly(screenshot);
+			FileUtils.moveFile(tempScreenshot, screenshot);
+			Thread.sleep(3000);
 
 		} catch (Exception ex) {
 			ex.printStackTrace();
